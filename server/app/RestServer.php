437fad8e555d2bbd06626a31d26e63c0f40bc0fc
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-11-01 18:18:24 +0200

    Update RestServer.php

--- server/app/RestServer.php
+++ server/app/RestServer.php
@@ -33,16 +33,15 @@ class RestServer extends DB
                     $this->param = $this->setMethod('delete'.ucfirst($dir), explode('/',  $index));
                 break;
                 case 'POST':
-                   //$put = json_decode(file_get_contents("php://input"), true);
-                    //$this->setMethod('put'.ucfirst($dir), explode('/', $index), $put);
-                   /* $putV = (explode('&', file_get_contents("php://input")));
+   
+                    $putV = (explode('&', file_get_contents("php://input")));
                     $put = array();
                     foreach ($putV as $value)
                     {
                         $keyValue = explode('=', $value);
                         $put[$keyValue[0]]=$keyValue[1];
-                    }*/
-                $this->setMethod('post'.ucfirst($dir), explode('/', $index));
+                    }
+                $this->setMethod('post'.ucfirst($dir), explode('/', $index), $put);
                 break;
                 case 'PUT':
                     $put = json_decode(file_get_contents("php://input"), true);
