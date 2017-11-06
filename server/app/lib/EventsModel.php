35c69123ba9afda3902b46e40cc2e7872fe2b32d
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-11-06 17:13:56 +0200

    Update EventsModel.php

--- server/app/lib/EventsModel.php
+++ server/app/lib/EventsModel.php
@@ -51,7 +51,8 @@ class EventsModel extends RestServer
 
     }
     public function addEvent($url,$param)
-    {
+    {  
+        $date->setTimezone(new DateTimeZone('Europe/Kiev ') );
         $idUser = $this->link->quote($param['id_user']);
         $idRoom= $this->link->quote($param['id_room']);
         $desc= $this->link->quote($param['description']);
